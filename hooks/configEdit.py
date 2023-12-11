d825bf8b470cd898869a7c2cd139b94ae74646e9
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-12-11 16:01:36 -0700

    Fix new domain

--- hooks/configEdit.py
+++ hooks/configEdit.py
@@ -6,6 +6,6 @@ def on_config(config: MkDocsConfig) -> MkDocsConfig | None:
 	pass
 
 def on_nav(nav: Navigation, config: MkDocsConfig, files: Files) -> Navigation | None:
-	nav.items.insert(0, Link(title="Main Site", url="https://chainfuse.com"))
+	nav.items.insert(0, Link(title="Main Site", url="https://chainfuse.ai"))
 
 	return nav
