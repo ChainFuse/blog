bbcb68f42efa4317470f756114f568a95b31df9b
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-12-11 16:35:01 -0700

    Main site still `.com`

--- hooks/configEdit.py
+++ hooks/configEdit.py
@@ -6,6 +6,6 @@ def on_config(config: MkDocsConfig) -> MkDocsConfig | None:
 	pass
 
 def on_nav(nav: Navigation, config: MkDocsConfig, files: Files) -> Navigation | None:
-	nav.items.insert(0, Link(title="Main Site", url="https://chainfuse.ai"))
+	nav.items.insert(0, Link(title="Main Site", url="https://chainfuse.com"))
 
 	return nav
